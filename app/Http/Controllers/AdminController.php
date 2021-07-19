33bc7a2e9c9243258367814a65889d8fd5ebf237
ggrach <ggrachdev@yandex.ru>
2021-07-19 11:20:32 +0400

    Initial commit

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Post;
+use App\Comment;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    //
+
+
+    public function index(){
+
+
+        $postsCount = Post::count();
+        $categoriesCount = Category::count();
+        $commentsCount = Comment::count();
+
+
+        return view('admin/index', compact('postsCount','categoriesCount','commentsCount'));
+
+    }
+
+
+}
---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Http\Requests;
+use App\Post;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $posts = Post::paginate(2);
+
+        $categories = Category::all();
+
+        $year = 2019;
+
+
+        return view('front/home', compact('posts', 'categories', 'year'));
+    }
+
+
+    public function post($slug){
+
+
+        $post = Post::findBySlugOrFail($slug);
+
+        $categories = Category::all();
+
+        $comments = $post->comments()->whereIsActive(1)->get();
+
+        return view('post', compact('post','comments', 'categories'));
+
+
+    }
+
+
+    public function welcome(){
+
+        return view('welcome');
+
+
+    }
+
+
+
+
+}
